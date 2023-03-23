f24b7df761a6209403614bd7e587bd79bb203b29
pedro.almeida <pedro.almeida@hartbgroup.com.br>
2023-03-22 22:55:12 -0300

    add arquivos

---
+++ Exemplos_aula_R.R
@@ -0,0 +1,40 @@
+### Produto de matrizes 
+a = c(2,-1,0,1,2,-3)
+A = matrix(a, byrow = T, nrow = 2, ncol = 3)
+A
+b = c(1,1,1,2,1,3)
+B = matrix(b, byrow = T, nrow = 3, ncol = 2)
+
+A%*%B
+
+## Produto de Kroncker
+
+a = c(1,0,0,1)
+A = matrix(a, byrow = T, nrow = 2, ncol = 2)
+A
+b = c(1,1,1, 1)
+B = matrix(b, byrow = T, nrow = 2, ncol = 2)
+
+kronecker( A, B, "*"  )
+
+
+### inversa de matriz 
+
+A = matrix( c(1,2,1,2,1,0,1,1,1), byrow = T, ncol = 3, nrow =3 )
+
+solve(A)
+
+### Decomposição espectral 
+
+A = matrix( c(4,2,2,2,2,0,2,0,2), ncol = 3, nrow =3 )
+A
+
+eig = eigen(A)
+
+lambda = eig$values
+LAMBDA = diag(lambda)
+U = eig$vectors
+
+U%*%LAMBDA%*%t(U)
+
+
---
+++ exemplo_densidades.R
@@ -0,0 +1,38 @@
+
+## funcao de densidade marginal da distribuição normal
+
+fdp_norm <- function( x, sigma2, mu ){
+  n = length(x)  
+  fx = ( (2*pi*sigma2)^(-1/2) )*exp(  (-1/(2*sigma2))*( (x - mu)^2  )   )
+  return(fx)
+}
+
+## Visualizando a curva de densidade
+
+## gerando dados normais
+par(mfrow = c(1, 2))
+
+x <- seq(70, 130, length.out = 100)
+ver <- veros( x, sigma2 = 10^2, mu = 100  )
+plot(x, ver, type = "l")
+
+
+x <- seq(70, 130, length.out = 100)
+fx <- dnorm(x, mean = 100, sd = 10)
+plot(x, fx, type = "l")
+
+
+
+## funcao de densidade conjunta de variaveis aleatorias independentes N(mu, sigma2):
+
+dens_conj_norm <- function( x, sigma2, mu ){
+  n = length(x)  
+  fx = ( (2*pi*sigma2)^(-n/2) )*exp(  (-1/(2*sigma2))*sum( (x - mu)^2  )   )
+  return(fx)
+}
+
+
+
+
+
+
